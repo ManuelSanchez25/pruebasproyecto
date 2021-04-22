3df1f5d57510b92e6745c4064f0a3248ec6df1dd
ManuelSanchez25 <manuel.vma12@gmail.com>
2021-04-22 13:00:52 -0500

    cambios

--- routes/project.js
+++ routes/project.js
@@ -1,26 +1,55 @@
 const express = require('express');
 const router = express.Router();
-const projectAPI  = require('./projectAPI');
-const json = require('../data/variables.json')
-const data = require('../data/variables.json');
-//pruebas
-//Declare a variable for init the information got of the .json
-var  variab  = projectAPI.variables;
-
-var auxVars = JSON.stringify(variab);
-
+const projectAPI = require('./projectAPI');
 
 // Get Login page
 router.get('/login', (req, res) => {
-    res.render('login');
-  });
-
+  res.render('login');
+});
 
 // Get home page
-router.get('/home', (req, res) => {
-    res.render('home', {variab})
-    console.log("varssss: "+ auxVars);
-  });
+router.get('/home', async function (req, res, next) {
+  let funcion = await projectAPI.fetchOneByKey();
+  let function2 = await projectAPI.litrosPorDia();
+  let funcion3 = await projectAPI.infoFarm();
+  var auxJSON = JSON.stringify(funcion);
+  var auxForV1 = [];
+  var litros =[];
+  var dia =[];
+  let lugar =[];
+  var irrigationTimeSec =[];
+  var cultivo =[];
+  for (let i = 0; i < funcion.length; i++) {
+    auxForV1.push(funcion[i].airHumidity)
+  }
+  for (let i = 0; i < function2.length; i++) {
+    litros.push(function2[i].litersUsed)
+  }
+  for (let i = 0; i < function2.length; i++) {
+    dia.push(function2[i].day)
+  }
+ 
+  for (let i = 0; i < function2.length; i++) {
+    irrigationTimeSec.push(function2[i].irrigationTimeSec)
+  }
+  for (let i = 0; i < funcion3.length; i++) {
+    lugar.push(funcion3[i].City)
+  }
+  for (let i = 0; i < funcion3.length; i++) {
+    cultivo.push(funcion3[i].farmName)
+  }
+  
+
+  console.log(auxForV1);
+  console.log(litros);
+  console.log(dia);
+  console.log(lugar);
+  res.render('home', {
+    auxForV1,litros,dia,irrigationTimeSec,lugar,cultivo
+  })
+  console.log("JSON from the table of SensorsData: " + auxJSON);
+}); 
+
 
 //Export for middlewares
 module.exports = router;
--- routes/projectAPI.js
+++ routes/projectAPI.js
@@ -1,17 +1,67 @@
-const data = require('../data/variables.json');
+var AWS = require("aws-sdk");
 
-let temp =[];
-let variables = [];
-for (let i = 0; i < data.length; i ++){
-    variables.push(data[i])
-}
-for (let j=0; j<data.length; j++){
-    temp[j] =variables[j].temperatura
-}
-console.log(variables)
-console.log(temp)
+let awsConfig = {
+    "aws_access_key_id" : "ASIAVQ3FWTFCPNZGMREF",
+"aws_secret_access_key" :"sf4d8Tjr6n5l/1ffpln5b8eCb8gS5gfuwMgVKte2",
+"region": 'us-east-1'
+};
+//var credentials = new AWS.SharedIniFileCredentials({ profile: 'default' });
+
+AWS.config.update( awsConfig );
+//AWS.config.credentials = credentials;
+let variabless
+let docClient = new AWS.DynamoDB.DocumentClient();
+
+let fetchOneByKey = async function  () {
+    var params = {
+        TableName: "sensorsData",
+        Key: {
+            "sensorDataId": '889eb999-1b89-4f06-bcda-9e379883e809'
+        }
+    };
+    let request = await docClient.scan(params);
+let respone = await request.promise();
+variabless = respone.Items
+//console.log(variabless)
+
+return variabless
+};
+let litrosPorDia = async function  () {
+    var params = {
+        TableName: "irrigations",
+        
+    };
+    let request = await docClient.scan(params);
+let respone = await request.promise();
+litrosgastados = respone.Items
+//console.log(variabless)
+
+return litrosgastados
+};
+
+let infoFarm = async function  () {
+    var params = {
+        TableName: "farms",
+        
+    };
+    let request = await docClient.scan(params);
+let respone = await request.promise();
+info = respone.Items
+//console.log(variabless)
+
+return info
+};
+
+
 
 module.exports = {
-    variables: variables,
-    temp: temp
+    fetchOneByKey:fetchOneByKey,
+    litrosPorDia: litrosPorDia,
+    infoFarm:infoFarm
 }
+/*
+https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/loading-node-credentials-shared.html
+de MIGUEL ALEJANDRO VILLA GUERRERO a todos:    8:58 AM
+https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/dynamodb-example-table-read-write.html
+de MIGUEL ALEJANDRO VILLA GUERRERO a todos:    8:58 AM
+https://www.netlify.com/guides/creating-an-api-with-aws-lambda-dynamodb-and-api-gateway/*/
