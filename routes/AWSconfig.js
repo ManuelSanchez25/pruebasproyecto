59848197e34002a60153d142ff4fed19552db4f8
ManuelSanchez25 <manuel.vma12@gmail.com>
2021-04-24 17:10:49 -0500

    cambios

---
+++ routes/AWSconfig.js
@@ -0,0 +1,7 @@
+module.exports = {
+    aws_remote_config: {
+      accessKeyId: 'ASIAVQ3FWTFCJQY62FTK',
+      secretAccessKey: 'gD3ENFfXFB1NKUr08ellq5xhMmo5U/xyW8bOv1Av',
+      region: 'us-east-1'
+    }
+};
--- routes/projectAPI.js
+++ routes/projectAPI.js
@@ -1,5 +1,7 @@
-var AWS = require("aws-sdk");
+let AWS = require("aws-sdk");
 require('dotenv').config();
+const config = require('./AWSconfig');
+
 
 //var credentials = new AWS.SharedIniFileCredentials({ profile: 'default' });
 /*const config = {
@@ -8,19 +10,16 @@ require('dotenv').config();
     region: "us-east-1",
 
 }*/
-var creds = new AWS.EnvironmentCredentials('AWS');
-creds.accessKeyId =='ASIAVQ3FWTFCO4YWR4PF';
-creds.secretAccessKey == 'I6N5oMy8PgbLY5ETw3ekToWEcUpsFQ4m3XEXi1NV'
-creds.sessionToken =='IQoJb3JpZ2luX2VjEI3//////////wEaCXVzLXdlc3QtMiJGMEQCIGC/3MWl1CVNofVOlnhRGT7XnyLNfH37mxmP3vfKzAjWAiAWGR0u8ef/d8PRFK0TucEvGKKltloV9Gh3mb9XVXN31yqzAgj2//////////8BEAAaDDM3OTc4MTAyODE2NCIMhB8hT4N9SyjJLa97KocCCiwBEmCyaO3DNbYZmTfJbjNgwLcb2AGGFmqC6x7QY8Dmce8HDpCWea4Jfg2Ui+H/L1RbZn5I2HAMANfXx0+UkwmqHybwi6RXaAJDgRw9CzFOEMJJPKNrsBetI90XR/bGWJJ76Bncuy2VdrvvbkmSpw6SptmJWknBzlGa43rzEG3QVNb6E/oCgCAkxOJ4LYF3PO+GKpWuwurbh7RDqY98ZUUFZoanEtYzL7xNVO/ttE28dXVqNtbITt7Auv38t4+GF+n1xzpS+BXW03FVG4BS/6OpweRIWvARmPlAwYc38wT+qd94vPihAHv3lEQz0bpa8jaKM4yiaRdt5NMp6CTvUals5ehJY/Aw1oeShAY6ngEIWpYjYmfJ1fpeJzpjh0+dTrWEDLluh1NLMucnxrUpHfi3Ad6W0Skyt5y4aYxkUuiKdDLQb1chLKPa26rGXBPdfom9jPzRFfdaAH1MV5iCX5OGwEi/XIC1hMo1rugVdhEy77BFJfonCaPvdsZa1kyfVr4aVd6GfZYvxB919Jm2odayudlQ4M3BrmoKV6H6nRgHClK5yZVgQ0TrO5hZmA=='
+
+AWS.config.update(config.aws_remote_config);
 
 
 // Replace ./data.json with your JSON feed
 
 //AWS.config.update({region :"us-east-1"});
-/AWS.config.update({region :"us-east-1"});
+//AWS.config.update({region :"us-east-1"});
 
 
-//AWS.config.credentials = creds;
 let variabless
 let docClient = new AWS.DynamoDB.DocumentClient();
 
